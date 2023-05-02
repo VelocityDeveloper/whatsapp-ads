a76f21214b3417b4105b1ace4af3c877dbf21aca
aadiityaak <aadiityaak@gmail.com>
2023-05-02 15:00:32 +0700

    add shortcode function

--- inc/function.php
+++ inc/function.php
@@ -89,6 +89,14 @@
 		);
 
 		add_settings_field(
+			'pesan_whatsapp', // id
+			'Pesan Whatsapp', // title
+			array( $this, 'pesan_whatsapp_callback' ), // callback
+			'whatsapp-ads-admin', // page
+			'whatsapp_ads_setting_section' // section
+		);
+
+		add_settings_field(
 			'tampilkan_whatsapp_melayang_1', // id
 			'Tampilkan Whatsapp melayang', // title
 			array( $this, 'tampilkan_whatsapp_melayang_1_callback' ), // callback
@@ -111,6 +119,10 @@
 			$sanitary_values['no_whatsapp_0'] = sanitize_text_field( $input['no_whatsapp_0'] );
 		}
 
+		if ( isset( $input['pesan_whatsapp'] ) ) {
+			$sanitary_values['pesan_whatsapp'] = sanitize_text_field( $input['pesan_whatsapp'] );
+		}
+
 		if ( isset( $input['tampilkan_whatsapp_melayang_1'] ) ) {
 			$sanitary_values['tampilkan_whatsapp_melayang_1'] = $input['tampilkan_whatsapp_melayang_1'];
 		}
@@ -130,6 +142,13 @@
 		printf(
 			'<input class="regular-text" type="text" name="whatsapp_ads[no_whatsapp_0]" id="no_whatsapp_0" value="%s">',
 			isset( $this->whatsapp_ads_options['no_whatsapp_0'] ) ? esc_attr( $this->whatsapp_ads_options['no_whatsapp_0']) : ''
+		);
+	}
+
+	public function pesan_whatsapp_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="whatsapp_ads[pesan_whatsapp]" id="pesan_whatsapp" value="%s">',
+			isset( $this->whatsapp_ads_options['pesan_whatsapp'] ) ? esc_attr( $this->whatsapp_ads_options['pesan_whatsapp']) : ''
 		);
 	}
 
@@ -158,3 +177,44 @@ if ( is_admin() )
  * $tampilkan_whatsapp_melayang_1 = $whatsapp_ads_options['tampilkan_whatsapp_melayang_1']; // Tampilkan Whatsapp melayang
  * $arahkan_ke_form_chat_2 = $whatsapp_ads_options['arahkan_ke_form_chat_2']; // Arahkan Ke form Chat
  */
+
+
+ add_action( 'wp_footer', 'wp_footer_whatsapp' );
+if(! function_exists('wp_footer_whatsapp')){
+    /**
+     * wp footer whatsapp floating
+     */
+    function wp_footer_whatsapp() {
+		$whatsapp_ads_options = get_option( 'whatsapp_ads' );
+		$tampilkan_whatsapp_melayang_1 = isset($whatsapp_ads_options['tampilkan_whatsapp_melayang_1']) ? $whatsapp_ads_options['tampilkan_whatsapp_melayang_1'] : ''; // Tampilkan Whatsapp melayang
+		$pesan_whatsapp = isset($whatsapp_ads_options['pesan_whatsapp']) ? $whatsapp_ads_options['pesan_whatsapp'] : 'Butuh bantuan?'; // Tampilkan Whatsapp melayang
+		$no_whatsapp = isset($whatsapp_ads_options['no_whatsapp_0']) ? $whatsapp_ads_options['no_whatsapp_0'] : '';
+		$arahkan_ke_form_chat = isset($whatsapp_ads_options['arahkan_ke_form_chat_2']) ? $whatsapp_ads_options['arahkan_ke_form_chat_2'] : ''; // Arahkan Ke form Chat
+		// Hilangkan semua karakter kecuali angka
+		$no_whatsapp = preg_replace('/[^0-9]/', '', $no_whatsapp);
+
+		// Ganti "0" di awal nomor dengan "62"
+		if (substr($no_whatsapp, 0, 1) == '0') {
+			$no_whatsapp = '62' . substr($no_whatsapp, 1);
+		}
+		if($arahkan_ke_form_chat) {
+			$no_whatsapp = get_site_url().'/form-chat';
+			$target = '';
+		} else {
+			$no_whatsapp = 'https://wa.me/'.$no_whatsapp;
+			$target = '_blank';
+		}
+        if($tampilkan_whatsapp_melayang_1) {
+            ?>
+            <div class="whatsapp-ads-floating right-bottom">
+                <a href="<?php echo $no_whatsapp; ?>" target="<?php echo $target; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                        <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                    </svg>
+                    <span><?php echo $pesan_whatsapp; ?></span>
+                </a>
+            </div>
+            <?php
+        }
+    }
+}
